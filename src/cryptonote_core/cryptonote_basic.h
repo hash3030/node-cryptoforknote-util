b1d2f4864bc106bb1d300e96b0aa793ee89743ea
hash3030 <43608884+hash3030@users.noreply.github.com>
2024-01-17 21:24:56 +1100

    Update cryptonote_basic.h

--- src/cryptonote_core/cryptonote_basic.h
+++ src/cryptonote_core/cryptonote_basic.h
@@ -978,6 +978,7 @@ namespace cryptonote
         }
       }
       if (blob_type == BLOB_TYPE_CRYPTONOTE_XLA && major_version >= 13) FIELD(signature)
+      if (blob_type == BLOB_TYPE_CRYPTONOTE_NEVO && major_version >= 14) FIELD(signature)
 
     END_SERIALIZE()
   };
